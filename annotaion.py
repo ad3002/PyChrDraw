a5c5fa168194ae30115f73062bfac59e14ba125f
Aleksey Komissarov <ad3002@gmail.com>
2013-10-14 20:24:43 +0400

    distribution

--- annotaion.py
+++ annotaion.py
@@ -5,6 +5,8 @@
 #@author: Aleksey Komissarov
 #@contact: ad3002@gmail.com
 
+from collections import defaultdict
+
 def read_snp_position_data(file_name):
     ''' Read SNP positions from file.
     @param file_name: file name with data (chr\tposition\n)
@@ -24,9 +26,10 @@ def compute_interval_freq(bands, chrs_lengths, bp_per_pixel=100000):
     @param bands: dictionary chr_name -> positions
     @param chrs_lengths: dictionary chr_name -> chr length
     @param bp_per_pixel: number nucleotides per pixel on image
-    @return: (chr2pos2freq, max_value)
+    @return: (chr2pos2freq, distribution)
     '''
     chr2pos2freq = {}
+    distribution = defaultdict(int)
     for name, length in chrs_lengths.items():
     	snps = bands[name]
     	snps.sort()
@@ -34,11 +37,12 @@ def compute_interval_freq(bands, chrs_lengths, bp_per_pixel=100000):
     	for snip in snps:
     		b = snip / bp_per_pixel
     		bins[b*bp_per_pixel] += 1
-    	max_value = float(max(bins.values()))
+    	for value in bins.values():
+    		distribution[value] += 1
     	chr2pos2freq[name] = bins
-    return chr2pos2freq, max_value
+    return chr2pos2freq, distribution
 
-def normalize_interval_freq(chr2pos2freq, max_cutoff=70):
+def normalize_interval_freq(chr2pos2freq, distribution, max_cutoff=70):
     ''' Compute normalized SNPs intervals for drawing.
     @param bands: dictionary chr_name -> positions
     @param chrs_lengths: dictionary chr_name -> chr length
@@ -53,3 +57,8 @@ def normalize_interval_freq(chr2pos2freq, max_cutoff=70):
     			chr2pos2freq[name][k] = max_cutoff
     		chr2pos2freq[name][k] /= max_cutoff
     return chr2pos2freq
+
+# if __name__ == '__main__':
+# 	file_name = "/Users/akomissarov/Downloads/cheetah.sSNP.tab/cheetah.sSNP.tab"
+# 	bands = read_snp_position_data(file_name)
+# 	
