7469067caa5d96c1f16b211a78362fadcc73271d
Aleksey Komissarov <ad3002@gmail.com>
2013-10-25 18:17:46 +0400

    It works!

--- draw_cats.py
+++ draw_cats.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
         settings = yaml.load(fh)
 
     cat_chrs = settings["chromosome_sizes"]
+    wild_sizes = settings["wild_sizes"]
     cheetah_chrs = settings["cheetah_chrs"]
     cheetah_to_cat = settings["cheetah_to_cat"]
     
@@ -39,28 +40,7 @@ if __name__ == '__main__':
     wild_cat_snipts_file = settings["wild_cat_snipts_file"]
 
     cat_to_cheetah = settings["cat_to_cheetah"]
-
-    data_chrs = []
-    max_value = 0.
-    
-    print "Load cats SNPs..."
-    cat_chr2bands = load_snips_from_bed(cat_snips_file)
-    cheetah_chr2bands = load_snips_from_bed(cheetah_snips_file)
-    wild_chr2bands = load_snips_from_bed(wild_cat_snipts_file)
-    
-    print "Cat..."
-    cat_chr2pos2freq, distribution = compute_interval_freq(cat_chr2bands, cat_chrs, bp_per_pixel=100000)
-    cat_chr2pos2freq = normalize_interval_freq(cat_chr2pos2freq, distribution, max_cutoff=70)
-    cat_bands = freqs_to_bands(cat_chr2pos2freq)
-
-    print "Cheetah"
-    ch_chr2pos2freq, distribution = compute_interval_freq(cheetah_chr2bands, cheetah_chrs, bp_per_pixel=100000)
-    ch_chr2pos2freq = normalize_interval_freq(ch_chr2pos2freq, distribution, max_cutoff=70)
-    ch_bands = freqs_to_bands(ch_chr2pos2freq)
-
-    wild_chr2pos2freq, distribution = compute_interval_freq(wild_chr2bands, cat_chrs, bp_per_pixel=100000)
-    wild_chr2pos2freq = normalize_interval_freq(wild_chr2pos2freq, distribution, max_cutoff=70)
-    wild_bands = freqs_to_bands(wild_chr2pos2freq)
+    cheetah_to_cat = settings["cheetah_to_cat"]
 
     x = settings["canvas"]["left_corner"]
     y = settings["canvas"]["top_corner"]
@@ -74,26 +54,50 @@ if __name__ == '__main__':
     x = settings["canvas"]["left_corner"]
     y = settings["canvas"]["top_corner"]
 
+    data_chrs = []
+    max_value = 0.
+    
+    print "Load cats SNPs..."
+    cat_chr2bands = load_snips_from_bed(cat_snips_file)
+    # cheetah_chr2bands = load_snips_from_bed(cheetah_snips_file)
+    wild_chr2bands = load_snips_from_bed(wild_cat_snipts_file)
+    
+    print "Cat..."
+    cat_chr2pos2freq, distribution = compute_interval_freq(cat_chr2bands, cat_chrs, bp_per_pixel=100000)
+    cat_chr2pos2freq = normalize_interval_freq(cat_chr2pos2freq, distribution, max_cutoff=70)
+    cat_bands = freqs_to_bands(cat_chr2pos2freq)
+
+    # print "Cheetah"
+    # ch_chr2pos2freq, distribution = compute_interval_freq(cheetah_chr2bands, cheetah_chrs, bp_per_pixel=100000)
+    # ch_chr2pos2freq = normalize_interval_freq(ch_chr2pos2freq, distribution, max_cutoff=70)
+    # ch_bands = freqs_to_bands(ch_chr2pos2freq)
+
+    print "Wild cat"
+    wild_chr2pos2freq, distribution = compute_interval_freq(wild_chr2bands, wild_sizes, bp_per_pixel=100000)
+    wild_chr2pos2freq = normalize_interval_freq(wild_chr2pos2freq, distribution, max_cutoff=210)
+    wild_bands = freqs_to_bands(wild_chr2pos2freq)
+
+    print "Fix coordinates..."
     cat_ready_chrs, cat_chr2bands = normalize_chromosome_sizes(cat_chrs, real_chr_size, bands=cat_bands)
-    ch_ready_chrs, ch_chr2bands = normalize_chromosome_sizes(cheetah_chrs, real_chr_size, bands=ch_bands)
-    w_ready_chrs, w_chr2bands = normalize_chromosome_sizes(cat_chrs, real_chr_size, bands=wild_bands)
+    # ch_ready_chrs, ch_chr2bands = normalize_chromosome_sizes(cheetah_chrs, real_chr_size, bands=ch_bands)
+    w_ready_chrs, w_chr2bands = normalize_chromosome_sizes(wild_sizes, real_chr_size, bands=wild_bands)
 
     for key in keys:
             print key
             length = cat_ready_chrs[key]
             draw_horizontal_chromosome(draw, x, y, length, bands=cat_chr2bands[key], chr_width=chr_width, name=key, scale=1, stars=None)
-            y += space_between_chr 
+            y += space_between_chr
 
             k = cat_to_cheetah[key]
-            length =ch_ready_chrs[k]
-            draw_horizontal_chromosome(draw, x, y, length, bands=ch_chr2bands[k], chr_width=chr_width, name=key, scale=1, stars=None)
+            # length =ch_ready_chrs[k]
+            # draw_horizontal_chromosome(draw, x, y, length, bands=ch_chr2bands[k], chr_width=chr_width, name=key, scale=1, stars=None)
         
-            y += space_between_chr 
+            # y += space_between_chr 
 
-            length = cat_ready_chrs[key]
-            draw_horizontal_chromosome(draw, x, y, length, bands=w_chr2bands[key], chr_width=chr_width, name=key, scale=1, stars=None)
+            length = w_ready_chrs[k]
+            draw_horizontal_chromosome(draw, x, y, length, bands=w_chr2bands[k], chr_width=chr_width, name=key, scale=1, stars=None)
     
 
             y += space_between_chr + 20
 
-    im.save("cat_output_file.png")
+    im.save("100k_cat_70_wild_210.png")
